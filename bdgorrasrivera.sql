81275d7a698152d837a2849e342594673c0f2192
REriveradelgadillo <143548741+REriveradelgadillo@users.noreply.github.com>
2023-10-26 13:43:12 -0600

    Add files via upload

---
+++ bdgorrasrivera.sql
@@ -0,0 +1,122 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 26-10-2023 a las 21:36:42
+-- Versión del servidor: 10.1.28-MariaDB
+-- Versión de PHP: 5.6.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bdgorrasrivera`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `login`
+--
+
+CREATE TABLE `login` (
+  `id` int(9) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `gmail` varchar(100) NOT NULL,
+  `usuario` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `login`
+--
+
+INSERT INTO `login` (`id`, `nombre`, `gmail`, `usuario`, `password`) VALUES
+(1, 'Emmanuel Rivera', 'emmanuel@gmail.com', 'rivera', '827ccb0eea8a706c4c34a16891f84e7b'),
+(4, 'jonathan Acevedo Castro', 'jonathan@gmail.com', 'Acevedo', '827ccb0eea8a706c4c34a16891f84e7b'),
+(6, 'David Arellano Gamon', 'david@gmail.com', 'david', '827ccb0eea8a706c4c34a16891f84e7b'),
+(7, 'Esperanza Ruiz Avila', 'morena@gmail.com', 'enanito', '202cb962ac59075b964b07152d234b70');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `products`
+--
+
+CREATE TABLE `products` (
+  `id` int(11) NOT NULL,
+  `pais` varchar(100) NOT NULL,
+  `id_cliente` int(5) NOT NULL,
+  `correo` varchar(50) NOT NULL,
+  `num_tarjeta` int(16) NOT NULL,
+  `tipo_tarjeta` varchar(10) NOT NULL,
+  `codigo_postal` int(5) NOT NULL,
+  `direccion` varchar(50) NOT NULL,
+  `ciudad` varchar(50) NOT NULL,
+  `login_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `products`
+--
+
+INSERT INTO `products` (`id`, `pais`, `id_cliente`, `correo`, `num_tarjeta`, `tipo_tarjeta`, `codigo_postal`, `direccion`, `ciudad`, `login_id`) VALUES
+(2, 'Mexico', 1, 'david@gmail.com', 2147483647, 'debito', 32584, 'melon #7207', 'juarez', 1),
+(3, 'Mexico', 1, 'morena', 2147483647, 'debito', 32584, 'del rio 8435', 'Juarez', 7);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `products`
+--
+ALTER TABLE `products`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK_products_1` (`login_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(9) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `products`
+--
+ALTER TABLE `products`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `products`
+--
+ALTER TABLE `products`
+  ADD CONSTRAINT `FK_products_1` FOREIGN KEY (`login_id`) REFERENCES `login` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ edit.php
@@ -0,0 +1,139 @@
+<?php session_start(); ?>
+
+<?php
+if(!isset($_SESSION['valid'])) {
+	header('Location: login.php');
+}
+?>
+
+<?php
+// including the database connection file
+include_once("connection.php");
+
+if(isset($_POST['update']))
+{	
+	$id = $_POST['id'];
+	
+	$pais = $_POST['pais'];
+	$id_cliente = $_POST['id_cliente'];
+	$correo = $_POST['correo'];	
+	$num_tarjeta = $_POST['num_tarjeta'];	
+	$tipo_tarjeta = $_POST['tipo_tarjeta'];	
+	$codigo_postal = $_POST['codigo_postal'];	
+	$direccion = $_POST['direccion'];	
+	$ciudad = $_POST['ciudad'];	
+	
+	// checking empty fields
+	if(empty($pais) || empty($id_cliente) || empty($correo) || empty($num_tarjeta) || empty($tipo_tarjeta) || empty($codigo_postal) || empty($direccion) || empty($ciudad)) {
+				
+		if(empty($pais)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+		
+		if(empty($id_cliente)) {
+			echo "<font color='red'>Quantity field is empty.</font><br/>";
+		}
+		
+		if(empty($correo)) {
+			echo "<font color='red'>correo field is empty.</font><br/>";
+		}	
+
+		if(empty($num_tarjeta)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+
+		if(empty($tipo_tarjeta)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+
+		if(empty($codigo_postal)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+
+		if(empty($direccion)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+
+		if(empty($ciudad)) {
+			echo "<font color='red'>Name field is empty.</font><br/>";
+		}
+				
+	} else {	
+		//updating the table
+		$result = mysqli_query($mysqli, "UPDATE products SET pais='$pais', id_cliente='$id_cliente', correo='$correo', num_tarjeta='$num_tarjeta', tipo_tarjeta='$tipo_tarjeta', codigo_postal='$codigo_postal', direccion='$direccion', ciudad='$ciudad'   WHERE id=$id");
+		
+		//redirectig to the display page. In our case, it is view.php
+		header("Location: view.php");
+	}
+}
+?>
+<?php
+//getting id from url
+$id = $_GET['id'];
+
+//selecting data associated with this particular id
+$result = mysqli_query($mysqli, "SELECT * FROM products WHERE id=$id");
+
+while($res = mysqli_fetch_array($result))
+{
+	$pais = $res['pais'];
+	$id_cliente = $res['id_cliente'];
+	$correo = $res['correo'];
+	$num_tarjeta = $res['num_tarjeta'];
+	$tipo_tarjeta = $res['tipo_tarjeta'];
+	$codigo_postal = $res['codigo_postal'];
+	$direccion = $res['direccion'];
+	$ciudad = $res['ciudad'];
+}
+?>
+<html>
+<head>	
+	<title>editar</title>
+</head>
+
+<body>
+	<a href="index.php">Inicio</a> | <a href="view.php">Ver</a> | <a href="logout.php">cerrar sesion</a>
+	<br/><br/>
+	
+	<form name="form1" method="post" action="edit.php">
+		<table border="0">
+			<tr> 
+				<td>pais</td>
+				<td><input type="text" name="pais" value="<?php echo $pais;?>"></td>
+			</tr>
+			<tr> 
+				<td>id cliente</td>
+				<td><input type="text" name="id_cliente" value="<?php echo $id_cliente;?>"></td>
+			</tr>
+			<tr> 
+				<td>correo</td>
+				<td><input type="text" name="correo" value="<?php echo $correo;?>"></td>
+			</tr>
+			<tr> 
+				<td>numero de tarjeta</td>
+				<td><input type="text" name="num_tarjeta" value="<?php echo $num_tarjeta;?>"></td>
+			</tr>
+			<tr> 
+				<td>tipo de tarjeta</td>
+				<td><input type="text" name="tipo_tarjeta" value="<?php echo $tipo_tarjeta;?>"></td>
+			</tr>
+			<tr> 
+				<td>codigo postal</td>
+				<td><input type="text" name="codigo_postal" value="<?php echo $codigo_postal;?>"></td>
+			</tr>
+			<tr> 
+				<td>direccion</td>
+				<td><input type="text" name="direccion" value="<?php echo $direccion;?>"></td>
+			</tr>
+			<tr> 
+				<td>ciudad</td>
+				<td><input type="text" name="ciudad" value="<?php echo $ciudad;?>"></td>
+			</tr>
+			<tr>
+				<td><input type="hidden" name="id" value=<?php echo $_GET['id'];?>></td>
+				<td><input type="submit" name="update" value="actualizar"></td>
+			</tr>
+		</table>
+	</form>
+</body>
+</html>
